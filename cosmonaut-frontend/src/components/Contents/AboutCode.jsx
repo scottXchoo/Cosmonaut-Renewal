10d5226f9476821ee1cfdfd41846c9c522c98db0
scottXchoo <ckh0601@cau.ac.kr>
2023-12-29 17:49:29 +0900

    test commit

--- cosmonaut-frontend/src/components/Contents/AboutCode.jsx
+++ cosmonaut-frontend/src/components/Contents/AboutCode.jsx
@@ -4,7 +4,7 @@ function AboutCode(props) {
   return (
     <>
       <h1 class="w-full text-3xl font-heading place-content-center mb-2">
-        {props.children}
+        {props.children}!
       </h1>
     </>
   );
